eed111a3bc2f05b357bc8b401de768737a755948
Davi Castilho <davi.lc@sempreceub.com>
2026-03-11 09:47:06 -0300

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -1,14 +1,37 @@
+package aula4_2;
+
+import java.util.ArrayList;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		Funcionario f1 = new Funcionario("Rodrigo",6500); 
-		Funcionario f2 = new Gerente("Pedro",10000);
-		Funcionario f3 = new Estagiario("Marcos",3500);
-		
-		f1.Resultado();
-		f2.Resultado();
-		f3.Resultado();
+
+		ArrayList<String> carros = new ArrayList<String>();
+		carros.add("Pulse");
+		carros.add("Honda Fit");
+		carros.add("147");
+		carros.add("Opala");
+		carros.add("Byd");
+
+		System.out.println(carros);
+
+		carros.remove(2);
+
+		System.out.println(carros);
+
+		carros.remove("Byd");
+
+		System.out.println(carros);
+
+		// acessando uma informação específica
+		System.out.println(carros.get(1));
+
+		carros.set(0, "Abarth");
+
+		System.out.println(carros);
+
+		for (String carro : carros) {
+			System.out.println(carro);
+		}
 	}
 }
