bb5092e60c1536a11902d47de1109c0e324dc5aa
davi.lc@sempreceub.com <davi.lc@sempreceub.com>
2026-03-11 08:39:23 -0300

    aula4

---
+++ src/aula4/Aluno.java
@@ -0,0 +1,33 @@
+package aula4;
+
+public class Aluno {
+
+	private String nome = "";
+	private String ra = "";
+	private int idade = 0;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRa() {
+		return ra;
+	}
+
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
---
+++ src/aula4/Principal.java
@@ -0,0 +1,16 @@
+package aula4;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Aluno a1 = new Aluno();
+		a1.setNome("Molina");
+
+		Aluno a2 = new Aluno();
+		a2.setNome("José");
+
+		System.out.println(a1.getNome().toUpperCase());
+		System.out.println(a2.getNome().toUpperCase());
+	}
+}
